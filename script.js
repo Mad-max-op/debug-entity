cda91101fff8687981a61ac755e3804dab35c93a
Nithesh24 <65641597+Nithesh24@users.noreply.github.com>
2023-05-18 13:53:24 +0530

    password hide un hide

---
+++ script.js
@@ -0,0 +1,18 @@
+let pass_image = document.getElementsByClassName("pass-eye")[0]
+let pass_box = document.getElementById("password")
+
+function pass_fun(e)
+{
+    let state = e.src
+    state = state.split("/")
+    state = state[state.length-1]
+
+    if(state == "hide.png")
+    {
+        pass_image.src = "assets/view.png"
+        pass_box.type = "text"
+    }else{
+        pass_image.src = "assets/hide.png"
+        pass_box.type = "password"
+    }
+}
